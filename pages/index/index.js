734dafde495a5c37f80a34885a97655ebb2c676c
fupengfei058 <fupengfei@yaochufa.com>
2017-03-01 22:44:35 +0800

    3.1
    Signed-off-by: fupengfei058 <fupengfei@yaochufa.com>

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,21 +6,6 @@ Page({
     remind: '加载中',
     _days: ['一','二','三','四','五','六','日'],
     _weeks : ['第一周','第二周','第三周','第四周','第五周','第六周','第七周','第八周','第九周','第十周','十一周','十二周','十三周','十四周','十五周','十六周','十七周','十八周','十九周','二十周'],    
-    _time: [ //课程时间与指针位置的映射，{begin:课程开始,end:结束时间,top:指针距开始top格数}
-      { begin: '0:00', end: '7:59', beginTop: -4, endTop: -4 },
-      { begin: '8:00', end: '9:40', beginTop: 0, endTop: 200 },
-      { begin: '9:41', end: '10:04', beginTop: 204, endTop: 204 },
-      { begin: '10:05', end: '11:45', beginTop: 208, endTop: 408 },
-      { begin: '11:46', end: '13:59', beginTop: 414, endTop: 414 },
-      { begin: '14:00', end: '15:40', beginTop: 420, endTop: 620 },
-      { begin: '15:41', end: '16:04', beginTop: 624, endTop: 624 },
-      { begin: '16:05', end: '17:45', beginTop: 628, endTop: 828 },
-      { begin: '17:46', end: '18:59', beginTop: 834, endTop: 834 },
-      { begin: '19:00', end: '20:40', beginTop: 840, endTop: 1040 },
-      { begin: '20:41', end: '20:49', beginTop: 1044, endTop: 1044 },
-      { begin: '20:50', end: '22:30', beginTop: 1048, endTop: 1248 },
-      { begin: '22:31', end: '23:59', beginTop: 1254, endTop: 1254 },
-    ],
     timelineTop: 0,
     scroll: {
       left: 0
@@ -40,141 +25,18 @@ Page({
     dates: [],     //本周日期
     teacher: false   //是否为教师课表
   },
-  //分享
-  onShareAppMessage: function(){
-    var name = this.data.name || app._user.we.info.name,
-        id = this.data.id || app._user.we.info.id;
-    return {
-      title: name + '的课表',
-      desc: 'We重邮 - 课表查询',
-      path: '/pages/core/kb/kb?id='+id+'&name='+name
-    };
-  },
+  
   onLoad: function(options){
     var _this = this;
     app.loginLoad(function(){
       _this.loginHandler.call(_this, options);
     });
   },
-  //让分享时自动登录
-  loginHandler: function(options){
-    var _this = this;
-    _this.setData({
-      'term': app._time.term,
-      'teacher': app._user.teacher
-    });
-    // onLoad时获取一次课表
-    var id = options.id || app._user.we.info.id;
-    if(!id){
-      _this.setData({
-        remind: '未绑定'
-      });
-      return false;
-    }
-    if(options.id && options.name){
-      _this.setData({
-        name: options.name
-      });
-    }
-    _this.get_kb(id);
-  },
+  
   onShow: function(){
-    var _this = this;
-    // 计算timeline时针位置
-    function parseMinute(dateStr){ return dateStr.split(':')[0]*60 + parseInt(dateStr.split(':')[1]); }
-    function compareDate(dateStr1,dateStr2){
-      return parseMinute(dateStr1) <= parseMinute(dateStr2);
-    }
-    var nowTime = app.util.formatTime(new Date(),'h:m');
-    _this.data._time.forEach(function(e,i){
-      if(compareDate(e.begin,nowTime) && compareDate(nowTime,e.end)){
-        _this.setData({
-          timelineTop: Math.round(e.beginTop + (e.endTop-e.beginTop)*(parseMinute(nowTime) - parseMinute(e.begin))/100)
-        });
-      };
-    });
-    //设置滚动至当前时间附近，如果周末为设置left为其最大值102
-    var nowWeek = new Date().getDay();
-    _this.setData({
-      'scroll.left': (nowWeek===6||nowWeek===0) ? 102 : 0
-    });
-  },
-  onReady: function(){
-    var _this = this;
-    //查询其他人课表时显示
-    if(_this.data.name){
-      wx.setNavigationBarTitle({
-        title: _this.data.name+'的课表'
-      });
-    }
-  },
-  scrollXHandle: function(e){
-    this.setData({
-      'scroll.left': e.detail.scrollLeft
-    });
-  },
-  showDetail: function(e){
-    // 点击课程卡片后执行
-    var _this = this;
-    var week = _this.data.week;
-    var dataset = e.currentTarget.dataset;
-    var lessons = _this.data.lessons[dataset.day][dataset.wid];
-    var targetI = 0;
-    lessons[dataset.cid].target = true;
-    if(week != '*'){
-      lessons = lessons.filter(function(e){
-        return e.weeks.indexOf(parseInt(week)) !== -1;
-      });
-    }
-    lessons.map(function(e,i){
-      if(lessons.length === 1){
-        e.left = 0;
-      }else{
-        //笼罩层卡片防止超出课表区域
-        //周一~周四0~3:n lessons.length>=2*n+1时，设置left0为-n*128，否则设置为-60*(lessons.length-1)；
-        //周日~周五6~4:n lessons.length>=2*(6-n)+1时，设置left0为-(7-n-lessons.length)*128，否则设置为-60*(lessons.length-1)；
-        var left0 = -60*(lessons.length-1);
-        if(dataset.day <= 3 && lessons.length >= 2*dataset.day+1){
-          left0 = -dataset.day*128;
-        }else if(dataset.day >= 4 && lessons.length >= 2*(6-dataset.day)+1){
-          left0 = -(7-dataset.day-lessons.length)*128;
-        }
-        e.left = left0+128*i;
-      }
-      return e;
-    });
-    lessons.forEach(function(e,i){
-      if(e.target){
-        targetI = i;
-        lessons[i].target = false;
-      }
-    });
-    if(!lessons.length){ return false; }
-    _this.setData({
-      targetX: dataset.day*129+35+8,
-      targetY: dataset.wid*206+Math.floor(dataset.wid/2)*4+60+8,
-      targetDay: dataset.day,
-      targetWid: dataset.wid,
-      targetI: targetI,
-      targetLessons: lessons,
-      targetLen: lessons.length,
-      blur: true
-    });
-  },
-  hideDetail: function(){
-    // 点击遮罩层时触发，取消主体部分的模糊，清空target
-    this.setData({
-      blur: false,
-      targetLessons: [],
-      targetX: 0,
-      targetY: 0,
-      targetDay: 0,
-      targetWid: 0,
-      targetI: 0,
-      targetLen: 0
-    });
     
   },
+  
   infoCardTap: function(e){
     var dataset = e.currentTarget.dataset;
     if(this.data.targetI == dataset.index){ return false; }
@@ -215,38 +77,7 @@ Page({
       startPoint: [e.touches[0].pageX, e.touches[0].pageY]
     });
   },
-  //滑动切换课程详情
-  bindMoveDetail: function(e){
-    var _this = this;
-    var curPoint = [e.changedTouches[0].pageX, e.changedTouches[0].pageY],
-        startPoint = _this.data.startPoint, i = 0;
-    if(curPoint[0] <= startPoint[0]){
-      if(Math.abs(curPoint[0]-startPoint[0]) >= Math.abs(curPoint[1]-startPoint[1])){   
-        if(_this.data.targetI != _this.data.targetLen - 1) {
-          i = 1;//左滑
-        }
-      }
-    }else{
-      if(Math.abs(curPoint[0]-startPoint[0]) >= Math.abs(curPoint[1]-startPoint[1])){    
-        if(_this.data.targetI != 0) {
-          i = -1;//右滑
-        }
-      }
-    }
-    if(!i){ return false; }
-    _this.setData({
-      targetI: parseInt(_this.data.targetI) + i
-    });
-  },
-  //点击左右按钮切换swiper
-  swiperChangeBtn: function(e){
-    var _this = this;
-    var dataset = e.currentTarget.dataset, i, data={};
-    if(dataset.direction == 'left'){ i = -1; }
-    else if(dataset.direction == 'right'){ i = 1; }
-    data[dataset.target] = parseInt(_this.data[dataset.target]) + i;
-    _this.setData(data);
-  },
+  
   get_kb: function(id){
     //数组去除指定值
     function removeByValue(array,val){
