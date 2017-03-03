0fac84fee9caa83e460360bf2cd5780fe4acdc33
fupengfei <fupengfei@yaochufa.com>
2017-03-03 18:09:48 +0800

    3.3

--- app.js
+++ app.js
@@ -25,6 +25,13 @@ App({
     }
   },
   globalData:{
-    userInfo:null
+    userInfo:null,
+    requestUrl : "https://route.showapi.com/255-1",
+    appid : "27982",
+    apiKey : "495a1755b3184e4f8dfe30f818eb1a5e",
+    tText : '29',
+    tImg : '10',
+    tAudio : '31',
+    tVideo : '41'
   }
 })
