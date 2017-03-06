b012b5da617bcc4a3ee8f2870d8a0a5d1e2c6eb6
fupengfei058 <fupengfei@yaochufa.com>
2017-03-07 00:18:28 +0800

    Signed-off-by: fupengfei058 <fupengfei@yaochufa.com>

--- pages/course/course.js
+++ pages/course/course.js
@@ -3,24 +3,8 @@
 var app = getApp();
 Page({
   data: {
+    listData:[],
     course_empty:true,
-    _days: ['一','二','三','四','五','六','日'],
-    _weeks : ['第一周','第二周','第三周','第四周','第五周','第六周','第七周','第八周','第九周','第十周','十一周','十二周','十三周','十四周','十五周','十六周','十七周','十八周','十九周','二十周'],    
-    _time: [ //课程时间与指针位置的映射，{begin:课程开始,end:结束时间,top:指针距开始top格数}
-      { begin: '0:00', end: '7:59', beginTop: -4, endTop: -4 },
-      { begin: '8:00', end: '9:40', beginTop: 0, endTop: 200 },
-      { begin: '9:41', end: '10:04', beginTop: 204, endTop: 204 },
-      { begin: '10:05', end: '11:45', beginTop: 208, endTop: 408 },
-      { begin: '11:46', end: '13:59', beginTop: 414, endTop: 414 },
-      { begin: '14:00', end: '15:40', beginTop: 420, endTop: 620 },
-      { begin: '15:41', end: '16:04', beginTop: 624, endTop: 624 },
-      { begin: '16:05', end: '17:45', beginTop: 628, endTop: 828 },
-      { begin: '17:46', end: '18:59', beginTop: 834, endTop: 834 },
-      { begin: '19:00', end: '20:40', beginTop: 840, endTop: 1040 },
-      { begin: '20:41', end: '20:49', beginTop: 1044, endTop: 1044 },
-      { begin: '20:50', end: '22:30', beginTop: 1048, endTop: 1248 },
-      { begin: '22:31', end: '23:59', beginTop: 1254, endTop: 1254 },
-    ],
 },
 onShow: function(options){
     if(!wx.getStorageSync('isLogin')){
@@ -47,7 +31,7 @@ onShow: function(options){
     success: function(res){
       wx.hideToast();
     if(res.statusCode == 200 && res.data.code == 200){
-      $this.setData({course_empty:false});
+      $this.setData({course_empty:false,listData:res.data.data});
       console.log(res);
     }else{
       wx.showModal({title: '加载失败', content: '请检查网络设置！', showCancel: false});
@@ -67,7 +51,5 @@ onLoad:function(){
 showCourse: function(e){
 
 },
-
-catchMoveDetail: function(){ /*阻止滑动穿透*/ },
 
 });
