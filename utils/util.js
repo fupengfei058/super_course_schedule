dea22353c4105b3e92f31591c098bc1417510c1d
fupengfei <fupengfei@yaochufa.com>
2017-09-08 15:18:29 +0800

    format

--- utils/util.js
+++ utils/util.js
@@ -7,7 +7,6 @@ function formatTime(date) {
   var minute = date.getMinutes()
   var second = date.getSeconds()
 
-
   return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
 }
 
@@ -15,7 +14,6 @@ function formatNumber(n) {
   n = n.toString()
   return n[1] ? n : '0' + n
 }
-
 
 module.exports = {
   formatTime: formatTime
